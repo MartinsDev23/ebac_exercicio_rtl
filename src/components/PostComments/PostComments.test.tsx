a2e7b37579a278f11cd0215186cd678b6316edc8
Daniel Martins <danmartins016@gmail.com>
2025-01-12 14:26:37 -0300

    Exercício resolvido
    Atualizando o repositório com a inserção de testes, proposto como exercício

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,27 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve inserir dois comentários', () => {
+
+        render(<PostComment />);
+
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: 'Comentário de teste'
+            }
+        });
+
+        fireEvent.click(screen.getByTestId('btn'));
+
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: 'Segundo comentario de teste'
+            }
+        });
+
+        fireEvent.click(screen.getByTestId('btn'));
+
+        expect(screen.getAllByTestId('campo-comentario')).toHaveLength(2);
+    })
 });
